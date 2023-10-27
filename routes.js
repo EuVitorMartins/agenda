7a3ffc85d2c38c8676294637956aad79903aef14
Vitor Martins <vitor.martins.1321@gmail.com>
2023-10-27 11:55:50 -0300

    Atualização  de 27/10/2023

--- routes.js
+++ routes.js
@@ -2,11 +2,15 @@ const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
 
 route.get('/', homeController.index );
 
 route.get('/login/index', loginController.index );
 route.post('/login/register', loginController.register );
+route.post('/login/login', loginController.login );
+route.get('/login/logout', loginController.logout );
 
+route.get('/contato/index', contatoController);
 
 module.exports = route;
--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,6 +1,7 @@
 const Login = require("../models/LoginModel.js");
 
 exports.index = (req, res) => {
+    if (req.session.user) { return res.render('logado') };
     res.render('login')
 }
 
@@ -25,4 +26,34 @@ exports.register = async function (req, res) {
         console.log(e);
         return res.render('404')
     }
+}
+
+
+exports.login = async function (req, res) {
+    try {
+        const login = new Login(req.body);
+        await login.login();
+
+        if (login.errors.length > 0) {
+            req.flash("errors", login.errors);
+            req.session.save(function () {
+                return res.redirect('/login/index');
+            });
+            return;
+        }
+
+        req.flash('success', 'Login com sucesso!');
+        req.session.user = login.user;
+        req.session.save(function () {
+            return res.redirect('back');
+        });
+    } catch (e) {
+        console.log(e);
+        return res.render('404')
+    }
+}
+
+exports.logout = (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
 }
