4a14efca7661858d4b7aae1db08ff957f48ed910
Vitor Martins <vitor.martins.1321@gmail.com>
2023-10-27 16:20:43 -0300

    Atualização  de 27/10/2023  as 16:50

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
 const contatoController = require('./src/controllers/contatoController');
+const { loginRequired } = require('./src/middlewares/middleware');
 
 route.get('/', homeController.index );
 
@@ -11,6 +12,7 @@ route.post('/login/register', loginController.register );
 route.post('/login/login', loginController.login );
 route.get('/login/logout', loginController.logout );
 
-route.get('/contato/index', contatoController);
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/index',contatoController.register);
 
 module.exports = route;
--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -1,4 +1,22 @@
+const { async } = require('regenerator-runtime');
+const Contato = require('../models/ContatoModel');
+
 exports.index = (req, res) => {
-    res.send('index');
+    res.render('contato');
+}
+
+exports.register = async (req, res) => {
+    const contato = new Contato(req.body);
+    await contato.register();
+
+    if (this.errors.length > 0) {
+        req.flash('errors', contato.errors);
+        req.session.save(() => { res.redirect('/contato/index') });
+        return
+    }
+
+    req.flash('success', 'Contato Criado com Sucesso.');
+    req.session.save(() => { res.redirect('back') });
+    
 }
 
